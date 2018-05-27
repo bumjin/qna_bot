a62a98adc77322c78500ddbded582afa9210645c
Beomjin Park <bumjin@gmail.com>
2018-05-27 20:40:23 +0900

    add QnA bot

---
+++ bothub/bot.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+from bothub_client.bot import BaseBot
+from .ini import Translate
+
+import csv
+
+class Bot(BaseBot):
+    """Represent a Bot logic which interacts with a user.
+
+    BaseBot superclass have methods belows:
+
+    * Send message
+      * self.send_message(message, chat_id=None, channel=None)
+    * Data Storage
+      * self.set_project_data(data)
+      * self.get_project_data()
+      * self.set_user_data(data, user_id=None, channel=None)
+      * self.get_user_data(user_id=None, channel=None)
+
+    When you omit user_id and channel argument, it regarded as a user
+    who triggered a bot.
+    """
+
+    def handle_message(self, event, context):
+        """Handle a message received
+
+        event is a dict and contains trigger info.
+
+        {
+           "trigger": "webhook",
+           "channel": "<name>",
+           "sender": {
+              "id": "<chat_id>",
+              "name": "<nickname>"
+           },
+           "content": "<message content>",
+           "raw_data": <unmodified data itself webhook received>
+        }
+        """
+        message = event.get('content')
+        if message == '/start':
+          self.send_start_message()
+        else:
+          self.describe_message(message)
+
+    def send_start_message(self):
+      self.send_message('QnA 챗봇입니다.')
+    
+    def describe_message(self, text):
+      t = Translate()
+      msg = t.ini_describe(text)
+      self.send_message(msg)
---
+++ bothub/ini.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import json
+import csv
+
+from urllib.request import Request
+from urllib.request import urlopen
+from urllib.parse import quote
+
+class Translate(object):
+ 
+  def __init__(self):
+    return
+    
+  def ini_describe(self, text):
+    try:
+      with open('bothub/data.csv', 'r') as raw:
+        lines = raw.readlines()
+        wrapper = csv.reader(lines)
+        description_text = []
+        text = text.lower()
+        for record in wrapper:
+          key = record[0]
+          defaultValue = record[1]
+          description = record[2]
+          if(key.lower().find(text) > -1 or description.lower().find(text) > -1):
+            #print('key:', key, ' value:', value,' description',description)
+            str = "key: {key} \ndefaultValue: {defaultValue}\n description: {description}".format(key=key, defaultValue=defaultValue, description=description)
+            description_text.append(str)
+          
+        return  ",".join(description_text)
+    except IOError as e:
+      return 'Error:{}'.format(e)
